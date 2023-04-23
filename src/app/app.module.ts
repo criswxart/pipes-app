55bafae11f72f19a4435f6dde37887ffb0b0be55
Cristian <criswxart@gmail.com>
2023-04-23 00:52:12 -0400

    se agrega estilo y menubar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -14,7 +14,7 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule,
+    SharedModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+  
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+      this.menuItems = [
+          {
+              label: 'Pipes de angular',
+              icon: 'pi pi-desktop',
+              items: [
+                { label: 'Textos y fechas', icon: 'pi pi-align-left' },
+                { label: 'Números', icon: 'pi pi-dollar' },
+                { label: 'No comunes', icon: 'pi pi-globe' },
+              ]
+          },
+          {
+            label: 'Pipes personalizados',
+            icon: 'pi pi-cog',
+          }
+      ];
+  }
+}
