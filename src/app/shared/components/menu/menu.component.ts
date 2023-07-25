0d50158c9f2697cd06bf3506af0b7a599b4f91fe
Cristian <criswxart@gmail.com>
2023-07-24 23:42:45 -0400

    integracion de flex y vista de pipe basicos

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -17,9 +17,9 @@ export class MenuComponent {
               label: 'Pipes de angular',
               icon: 'pi pi-desktop',
               items: [
-                { label: 'Textos y fechas', icon: 'pi pi-align-left' },
-                { label: 'Números', icon: 'pi pi-dollar' },
-                { label: 'No comunes', icon: 'pi pi-globe' },
+                { label: 'Textos y fechas', icon: 'pi pi-align-left', routerLink: '/' },
+                { label: 'Números', icon: 'pi pi-dollar', routerLink: 'number' },
+                { label: 'No comunes', icon: 'pi pi-globe', routerLink: 'uncommon' },
               ]
           },
           {
